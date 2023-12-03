5208b04b839afc4f7664d9033545ce53a169312a
Achille Triomphe <achille.triomphe@epfl.ch>
2023-12-03 12:08:34 +0100

    Last Fix

--- lauzhackPolls/lauzhackPolls/lauzhackPollsApp.swift
+++ lauzhackPolls/lauzhackPolls/lauzhackPollsApp.swift
@@ -6,33 +6,36 @@
 //
 //
 
+import FirebaseCore
+import FirebaseFirestore
+import SwiftUI
 
-import UIKit
-import Firebase
-
-@UIApplicationMain
-class lauzhackPollsApp: UIResponder, UIApplicationDelegate {
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        let settings = Firestore.firestore().settings
 
+        // Uncomment this settings configuration to not use Local Emulator suite
+        // settings.host = "127.0.0.1:8080"
+        // settings.cacheSettings = MemoryCacheSettings()
+        // settings.isSSLEnabled = false
+        Firestore.firestore().settings = settings
         return true
     }
+    
+}
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+@main
+struct lauzhackPollsApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                HomeView()
+            }
+        }
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
